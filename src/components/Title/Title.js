594202a39938488a63b347b4952c2457d35213ac
Valli Avudaiappan <78044816+CodeFriend148@users.noreply.github.com>
2021-07-23 03:46:29 -0400

    Update Title.js

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,8 +4,15 @@ import React from 'react';
 export function Title(){
 
     return (
-        <div>
-            Title
+        <div className="aboutSection">
+            <div className="container">
+                <div className="about_inner">
+                    <div className="Col1"></div>
+                    <div className="Col2">
+                        <h1>Hi, I'm Nick</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
